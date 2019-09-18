76cbdda6e83ba7bc2793f2b3c48c1b493b7ec5ff
hamseungyup <55345826+hamseungyup@users.noreply.github.com>
2019-09-18 22:02:04 +0900

    Update test_init_final.py

--- test_init_final.py
+++ test_init_final.py
@@ -1340,9 +1340,14 @@ async def on_message(msg):
 				information = '``` ```'
 
 			embed = discord.Embed(
-					title = "----- 보스탐 정보 -----",
-					description=information,
+					title = "----- 고 정 보 스 -----",
+					description= fixed_information,
 					color=0x0000ff
+					)
+			embed.add_field(
+					name="----- 보스탐 정보 -----",
+					value= information,
+					inline = False
 					)
 			embed.add_field(
 					name="----- 미예약 보스 -----",
